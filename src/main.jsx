4bba0672ffd4bbb7a9e68b2864a089f4d62cbca5
joaquinxtx <thejotex1998@gmail.com>
2022-10-12 21:20:37 -0300

    Deployed

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { store } from './store/store'
 import { Provider } from 'react-redux'
 
 import { JournalApp } from './JournalApp'
-import './style.css'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
